b2a94bcd63dd06bff70880e178682ead350b44e7
fridler <fridler@gmail.com>
2021-04-11 19:13:58 +0300

    changed name

--- src/pages/ActorGalleryPage/ActorGalleryPage.js
+++ src/pages/ActorGalleryPage/ActorGalleryPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 // import './ActorGallery.css'
+import { Container } from 'react-bootstrap';
+
 
 function ActorGalleryPage(props) {
 
@@ -7,13 +9,12 @@ function ActorGalleryPage(props) {
 
 
     return (
-        <div className="p-actorGallery">
-            <div>
-                <img src="https://i.pinimg.com/474x/7b/42/c5/7b42c553d66d8b58970fadb14ead5f69.jpg" alt="" />
-                <p>name:</p>
-                <p>age:</p>
-            </div>
-        </div>
+        <Container className="p-actorGallery">
+            <img src="https://i.pinimg.com/474x/7b/42/c5/7b42c553d66d8b58970fadb14ead5f69.jpg" alt="" />
+            <p>name:</p>
+            <p>age:</p>
+        </Container>
+
     )
 }
 
